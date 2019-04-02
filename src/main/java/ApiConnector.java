25a537a4c77406f35a75540d5207fd5ebd724262
mcbarlowe <mcbarlowe@gmail.com>
2019-04-02 00:25:59 -0400

    Addded method to write API output to json file

--- src/main/java/ApiConnector.java
+++ src/main/java/ApiConnector.java
@@ -1,9 +1,15 @@
+import java.io.File;
 import java.io.IOException;
 import java.net.URLConnection;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 
 class ApiConnector {
     static final String base_url = "https://www.alphavantage.co/query?";
@@ -11,21 +17,25 @@ class ApiConnector {
     //reads in the data from the API when based a valid api url string
     String get_request(String url, int timeout){
         try {
+            int c;
             URL request = new URL(url);
             URLConnection conn = request.openConnection();
             conn.setConnectTimeout(timeout);
             conn.setReadTimeout(timeout);
+            String file = "test.json";
 
-            InputStreamReader inputStream = new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8);
-            BufferedReader bufferedReader = new BufferedReader(inputStream);
-            StringBuilder responseBuilder = new StringBuilder();
 
-            while (bufferedReader.readLine() != null) {
-                responseBuilder.append(bufferedReader.readLine());
+            InputStreamReader inputStream = new InputStreamReader(request.openStream(), StandardCharsets.UTF_8);
+            //BufferedReader bufferedReader = new BufferedReader(inputStream);
+            //StringBuilder responseBuilder = new StringBuilder();
+            //responseBuilder.append(bufferedReader.readLine());
+            while((c=inputStream.read())!=-1)
+            {
+                System.out.print((char)c);
             }
-            bufferedReader.close();
-            System.out.println(responseBuilder.toString());
-            return responseBuilder.toString();
+            Files.copy(request.openStream(),Paths.get(file), StandardCopyOption.REPLACE_EXISTING);
+
+            return null;
         }
         catch (IOException e) {
             throw new RuntimeException("Error pulling data from Alpha Vantage", e);
--- src/main/java/intraDayApiConnector.java
+++ src/main/java/intraDayApiConnector.java
@@ -19,6 +19,6 @@ class intraDayApiConnector extends ApiConnector{
         intraDayApiConnector intraday = new intraDayApiConnector("TIME_SERIES_INTRADAY", "MSFT", "5min", "BKWGHZ46RDAVZQOK");
         test_url = intraDayApiConnector.base_url + "function="+ intraday.function + "&symbol=" + intraday.symbol + "&interval=" + intraday.interval + "&apikey=" + intraday.apikey;
         System.out.println(test_url);
-        intraday.get_request(test_url, 10);
+        intraday.get_request(test_url, 1000);
     }
 }
