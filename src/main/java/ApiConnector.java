fd4ded8bb36cd8969a39d8382cb64a5974c28cf3
mcbarlowe <mcbarlowe@gmail.com>
2019-04-03 20:56:18 -0400

    added method to check and make sure the class variables are entered properly on StockTimeSeries class instantiation

--- src/main/java/ApiConnector.java
+++ src/main/java/ApiConnector.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -11,49 +9,51 @@ import java.nio.file.StandardCopyOption;
 
 class ApiConnector {
     static final String base_url = "https://www.alphavantage.co/query?";
+    private InputStream inputStream;
 
     /**
-     * Method reads in the data from the Alpha Vantage API
-     * @param url This is the API url to access the data
+     * Method reads in the data from the Alpha Vantage API and stores it in an
+     * InputStream class variable called inputStream
+     *
+     * @param url     This is the API url to access the data
      * @param timeout An int that specifies timeout in milliseconds
-     * @return inputStream an InputStream object that can be written to file
-     *                               or inserted into a SQL database
      */
-    InputStream get_request(String url, int timeout) {
+    void get_request(String url, int timeout) {
         InputStream inputStream;
         try {
             URL request = new URL(url);
             URLConnection conn = request.openConnection();
             conn.setConnectTimeout(timeout);
             conn.setReadTimeout(timeout);
-            inputStream = conn.getInputStream();
-
             //inputStream = new InputStreamReader(request.openStream(), StandardCharsets.UTF_8);
             //BufferedReader bufferedReader = new BufferedReader(inputStream);
             //StringBuilder responseBuilder = new StringBuilder();
             //responseBuilder.append(bufferedReader.readLine());
 
-            return inputStream;
+            this.inputStream = conn.getInputStream();
         } catch (IOException e) {
             throw new RuntimeException("Error pulling data from Alpha Vantage", e);
         }
     }
 
-    /**
-     * Method takes InputStream object and writes it to fileName
-     * @param inputStream InputStream object returned from the Alpha Vantage API.
-     * @param fileName    name of file to write InputStream object to.
-     */
-    void write_file (InputStream inputStream, String fileName){
 
+    /**
+     * Method takes inputStream class variable and writes it to fileName
+     * will overwrite file if a file with the same name exists in folder
+     *
+     * @param fileName name of file to write InputStream object to.
+     */
+    void write_file(String fileName) {
         int c;
         try {
-            while ((c = inputStream.read()) != -1) {
+            while ((c = this.inputStream.read()) != -1) {
                 System.out.print((char) c);
             }
-            Files.copy(request.openStream(), Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
-        }
-        catch (IOException e) {
+            Files.copy(this.inputStream, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
             throw new RuntimeException("Error writing Data from Alpha Vantage to file " + fileName, e);
         }
     }
+}
+
+
--- src/main/java/intraDayApiConnector.java
+++ src/main/java/intraDayApiConnector.java
@@ -1,23 +1,55 @@
-class intraDayApiConnector extends ApiConnector{
+import java.util.Arrays;
 
-    private final String function;
-    private final String symbol;
-    private final String interval;
-    private final String apikey;
+class StockTimeSeries extends ApiConnector{
+
+    private String function;
+    private String symbol;
+    private int[] interval;
+    private String apikey;
+    private String[] sizeValues = {"full", "compact"};
+    private String outputSize;
 
     //constructor
-    intraDayApiConnector(String function, String symbol, String interval, String apikey){
-        super();
+    StockTimeSeries(String function, String symbol, String apikey, String outputSize, int... interval){
+        checkInterval(function, symbol, apikey, outputSize, interval);
+    }
+
+    public urlBuilder(String function, String symbol, String apikey, String outputSize, int... interval){
+
+    }
+
+    /**
+     * This checks to make sure the proper interval is entered to the API and throws and exception
+     * if not when the StockTimeSeries object is instantiated
+     * @param function the type of time series API call you want object to make
+     * @param symbol   The Stock symbol you want to query against the Alpha Vantage API
+     * @param apikey   Your personal API key provided by Alpha Vantage
+     * @param interval This is the time interval between two data points
+     * @param outputSize Sets the output size of the API call compact returns just last 100 data points
+     */
+    public void checkInterval(String function, String symbol,  String apikey, String outputSize, int... interval) {
+        if (interval.length == 0 && function == "TIME_SERIES_INTRADAY"){
+            throw new IllegalArgumentException("Intraday Time Series API calls must have a time interval argument provided");
+        }
+        if (interval.length != 1){
+            throw new IllegalArgumentException("Too many time interval arguments passed to StockTimeSeries instance.");
+        }
+        if (!Arrays.asList(1, 5, 15, 30, 60).contains(interval)) {
+            throw new IllegalArgumentException("Invalid time interval passed to StockTimeSeries instance.");
+        }
+        if (!Arrays.asList(sizeValues).contains(outputSize)){
+            throw new IllegalArgumentException("Invalid outputSize passed to StockTimeSeries instance.");
+        }
         this.function = function;
         this.symbol = symbol;
         this.interval = interval;
         this.apikey = apikey;
+        this.outputSize = outputSize;
     }
-
     public static void main(String[] args) {
         String test_url;
-        intraDayApiConnector intraday = new intraDayApiConnector("TIME_SERIES_INTRADAY", "MSFT", "5min", "BKWGHZ46RDAVZQOK");
-        test_url = intraDayApiConnector.base_url + "function="+ intraday.function + "&symbol=" + intraday.symbol + "&interval=" + intraday.interval + "&apikey=" + intraday.apikey;
+        StockTimeSeries intraday = new StockTimeSeries("TIME_SERIES_INTRADAY", "MSFT",  "BKWGHZ46RDAVZQOK", 5);
+        test_url = StockTimeSeries.base_url + "function="+ intraday.function + "&symbol=" + intraday.symbol + "&interval=" + intraday.interval + "&apikey=" + intraday.apikey;
         System.out.println(test_url);
         intraday.get_request(test_url, 1000);
     }
